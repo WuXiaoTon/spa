aae58a2161b2df11011af22590519a1e014cb144
WuXiaoTon <974124343@qq.com>
2018-03-13 16:56:29 +0800

    WuXiaoTon

--- W05/progress.js
+++ W05/progress.js
@@ -1,9 +1,11 @@
 var pro=document.getElementById("progress");
+var x=1;
 var progress=function(obj){
   var i=pro.value;
-  if(obj.value=="begin"){
+  if(obj.value=="begin"&&x==1){
     console.log(i);
     time=window.setInterval(function(){i++;pro.value=i;},100);
+    x=2;
   }
   else if(obj.value=="stop"){
     time=window.clearInterval(time);
