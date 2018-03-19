1c1e5cb13cf5de75a7ab9e951718d0e6ae94ad24
WuXiaoTon <974124343@qq.com>
2018-03-19 15:17:51 +0800

    WuXiaoTon

--- W06/field.js
+++ W06/field.js
@@ -1,19 +1,25 @@
 var aler=function(){
   var width=document.getElementById('rectangle-width');
   var one=document.getElementById('one');
-var reg=/^\d+(\.\d+)?$/;
+  var reg=/^\d+(\.\d+)?$/;
+  var widths=String(width.value);
   if(width.value==""){
     one.innerHTML='宽度不能为空！';
     width.focus();
   }
-else if(reg.test(width.value)==false){
-  one.innerHTML="请输入数值！";
+  else if(reg.test(width.value)==false){
+    if(width.value[0]=="-"){
+    one.innerHTML='请输入大于0的数！';
+    }
+    else{
+    one.innerHTML='请输入数值！';
+    }
     width.focus();
-}
-else if(width.value<0){
-  one.innerHTML="请输入大于0的数！";
+  }
+  else if(widths[0]=="0"){
+    one.innerHTML='请输入大于0的数！';
     width.focus();
-}
+  }
   else{
      one.innerHTML='';
   }
@@ -21,19 +27,25 @@ else if(width.value<0){
 var ale=function(){
 var height=document.getElementById('rectangle-height');
 var two=document.getElementById('two');
-var reg=/^\d+(\.\d+)?$/;
+  var reg=/^\d+(\.\d+)?$/;
+  var heights=String(height.value);
 if (height.value==""){
   two.innerHTML="高度不能为空！";
   height.focus();
 }
-else if(reg.test(height.value)==false){
-  two.innerHTML="请输入数值！";
-  height.focus();
-}
-else if(height.value<0){
-  two.innerHTML="请输入大于0的数！";
+  else if(reg.test(height.value)==false){
+    if(height.value[0]=="-"||height.value[0]=="0"){
+    two.innerHTML='请输入大于0的数！';
+    }
+    else{
+    two.innerHTML='请输入数值！';
+    }
     height.focus();
-}
+  }
+  else if(heights[0]=="0"){
+    two.innerHTML='请输入大于0的数！';
+    height.focus();
+  }
 else{
   two.innerHTML="";
 }
